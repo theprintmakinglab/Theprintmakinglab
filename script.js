44d0b7509420eaabb5a42a4e68c3f2de8a73367f
The Printmaking Lab <mycoartlab@gmail.com>
2026-09-24 19:58:07 +0530

    Add artwork slideshow script

---
+++ script.js
@@ -0,0 +1,38 @@
+const artworks = [
+    {
+        image: "images/long-necked-man.jpg",
+        title: "Long-necked Man",
+        artist: "Jagath Weerasinhae",
+        details: "1989 · Woodblock print"
+    },
+    {
+        image: "images/things-shadow-raging-sea.jpg",
+        title: "Things, Shadow and Raging Sea",
+        artist: "Prasanna Ranabahu",
+        details: "2019 · Photo print · 66.5 × 42.1 cm"
+    },
+    {
+        image: "images/broken-hand-iii.jpg",
+        title: "Broken Hand III – The Peales Bomb Went Off",
+        artist: "Pradeep Chandrasiri",
+        details: "1999 · Photo print"
+    },
+    {
+        image: "images/perforated-body.png",
+        title: "Perforated Body",
+        artist: "Bandu Manamperi",
+        details: "2008 · Sheet Size 4R"
+    },
+    {
+        image: "images/story-of-hammer-and-nails.jpeg",
+        title: "The Story of Hammer and Nails",
+        artist: "Anusha Gajaweera",
+        details: "2018 · Mixed media on acid-free paper · 50 × 36 cm"
+    },
+    {
+        image: "images/what-remains.jpg",
+        title: "What Remains",
+        artist: "Buddhika Nakandala",
+        details: "2026 · Monotype · 31 × 45 cm · Acid-free paper"
+    }
+];
